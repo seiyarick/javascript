90132f583987261b56be5bfc0fd5b33795570748
seiyarick <seiyahirota2@gmail.com>
2022-05-23 12:09:21 +0900

    じゃんけん

--- app.js
+++ app.js
@@ -106,7 +106,178 @@
 // alert('１から１０まで足した結果は' + num + 'です');
 
 // 確認問題
-alert(3 + 5);
-alert(3 - 5);
-alert(3 * 5);
-alert(3 % 5);
+// alert(3 + 5);
+// alert(3 - 5);
+// alert(3 * 5);
+// alert(3 % 5);
+
+// JavaScriptの変数宣言を学ぼう
+
+// 再宣言
+// // varによる再宣言
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// // letによる再宣言
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+// =>Uncaught SyntaxError: Identifier 'nickname' has already been declared」
+
+// constによる再宣言
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+// =>Uncaught SyntaxError: Identifier 'nickname' has already been declared」
+
+// 再代入
+// varによる再代入
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname) 
+
+// // letによる再代入
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro!"
+// console.log(nickname)
+
+// constによる再代入
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro!"
+// console.log(nickname)
+
+// スコープ
+// varのスコープ
+
+// var str = "webcamp"
+
+// function foo(){
+//   console.log(str)
+//   var y = "hello"
+// }
+// foo()
+// console.log(y)
+
+// letのスコープ
+// function foo(){
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+// for(let i = 0;i < 10 ; i++){
+//   console.log(i)
+// }
+
+// console.log(i)
+
+// 巻き上げ
+
+// var str = "webcamp"
+
+// function foo(){
+//   console.log(str)
+//   var str = "dmm webcamp"
+//   console.log(str)
+// }
+
+// foo()
+
+
+// function addString(name){
+//   let adc = "Hello" + name;
+//   return adc;
+// }
+
+// let alertString;
+// alertString = addString("webcamp");
+
+// alert(alertString);
+
+// let promptStr = prompt('好きな数字を入力してください')
+// alert(promptStr);
+
+// let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+// alert('あなたの選んだ手は' + user_hand + 'です')
+
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random()*3)
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  }else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  }else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+  return hand_name;
+}
+
+// ユーザーの手とJavaScriptの手を比べる関数
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if (js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if (js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+      }else if (js == 'チョキ'){
+        winLoseStr = "あいこ";
+      }else if (js == "パー"){
+        winLoseStr = "勝ち";
+      }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    }else if (js == "チョキ"){
+      winLoseStr = "負け";
+    }else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+  
+  return winLoseStr;
+}
+
+
+
+// じゃんけんの手を入力するためのプロンプト欄を作成
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー、チョキ、パーのいずれかを入力してください');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+}
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
+
+// ユーザーの手とJavaScriptの手を比べる関数を呼び出し、結果をjudgeする
+let judge = winLose(user_hand, js_hand)
+
+// 結果を表示する
+if (user_hand != null){
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+}else{
+  alert("またチャレンジしてね");
+}
+
+
